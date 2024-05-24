d1216eaaa81d2f7b61c75f84e81d6cbc581ac0c9
kalaws <90693616+kalaws@users.noreply.github.com>
2024-05-24 12:21:13 +0200

    Update __init__.py

--- sungrow_websocket/__init__.py
+++ sungrow_websocket/__init__.py
@@ -84,17 +84,17 @@ class SungrowWebsocket:
             if d["result_code"] != 1 or d["result_msg"] != "success":
                 return data
             token: str = d["result_data"]["token"]
-            print(token)
+            
             await websocket.send(
                 json.dumps(
-                    {"lang": self.locale, "token": "", "service": "login", "username": "admin", "passwd": "pw8888"}
+                    {"lang": self.locale, "token": token, "service": "login", "username": "admin", "passwd": "pw8888"}
                 )
             )
             d: Result = json.loads(await websocket.recv())
             if d["result_code"] != 1 or d["result_msg"] != "success":
                 return data
             token: str = d["result_data"]["token"]
-            print(token)
+            #print(d)
             await websocket.send(
                 json.dumps(
                     {
